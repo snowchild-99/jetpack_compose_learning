a4224cb6db3a97279fc8bdc29707199b60376f4e
snowchild-99 <sandeepyadavxoxo@gmail.com>
2023-05-30 18:23:38 +0530

    state change & clickable

--- app/src/main/java/com/snowchild/jetpackcomposelearning/MainActivity.kt
+++ app/src/main/java/com/snowchild/jetpackcomposelearning/MainActivity.kt
@@ -1,46 +1,57 @@
-    package com.snowchild.jetpackcomposelearning
+package com.snowchild.jetpackcomposelearning
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.snowchild.jetpackcomposelearning.ui.theme.JetpackcomposelearningTheme
+import androidx.compose.ui.graphics.Color
+import java.util.Random
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnrememberedMutableState")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackcomposelearningTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("JetPackCompose")
-                }
+            val color = remember{
+                mutableStateOf(Color.Cyan)
             }
+            Column (modifier = Modifier
+                .fillMaxSize()){
+                ColorBox(Modifier.weight(0.5f).fillMaxSize()){
+                    color.value = it
+                }
+                Box(modifier = Modifier
+                    .weight(0.5f)
+                    .fillMaxSize(1f)
+                    .background(color = color.value))
+            }
+
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetpackcomposelearningTheme {
-        Greeting("JetPackCompose")
+        @Composable
+        fun ColorBox(modifier: Modifier = Modifier,
+        updateColor : (Color) -> Unit) {
+            Box(modifier = modifier
+                .background(Color.Red)
+                .clickable {
+                    updateColor(Color(
+                        kotlin.random.Random.nextInt(),
+                    )
+                    )
+                }
+
+            )
+        }
     }
-}
